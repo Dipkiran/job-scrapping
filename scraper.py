3c91dd35e24ec15d6e51187fe3d44341731a338e
Dipkiran <071bct516dip@pcampus.edu.np>
2018-08-13 22:41:28 +0545

    newone

--- scraper.py
+++ scraper.py
@@ -1,6 +1,11 @@
 from bs4 import BeautifulSoup
 
 import requests
+import csv
+
+outfile = open('career.csv','w')
+writer = csv.writer(outfile)
+writer.writerow(["job_link", "job_desc"])
 
 r = requests.get("http://jkssb.nic.in/WriteReadData/File/Home.htm")
 
@@ -13,7 +18,10 @@ article = soup.find("div", {"class": "top"}).findAll('li')
 i = 1
 for element in article:
     for a in element.find_all('a', href=True):
-        print a.text
-        print "http://jkssb.nic.in/" + a['href']
+        if i <11:
+            item_link =  a.text
+            item_text = "http://jkssb.nic.in/" + a['href']
+            writer.writerow([item_link, item_text])
+            i+=1
 
-
+outfile.close()
